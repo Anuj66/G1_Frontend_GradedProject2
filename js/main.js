1327233af072ad2cedcc92b3ccbd87ac48dc4f65
anuj66 <saraogi.anuj9@gmail.com>
2023-01-21 16:27:03 +0530

    Updated styling

--- js/main.js
+++ js/main.js
@@ -3,6 +3,15 @@ let appliedFor = document.getElementById("applied-for");
 let technicalSkillsUl = document.getElementById("technical-skills-ul");
 let hobbiesUl = document.getElementById("hobbies-ul");
 let workExperienceDiv = document.getElementById("work-experience-div");
+let resumeName = document.getElementById("resume-name");
+let phoneNo = document.getElementById("phone-no");
+let email = document.getElementById("email");
+let linkedin = document.getElementById("linkedin-href");
+let companyName = document.getElementById("company-name-p");
+let companyPosition = document.getElementById("position-p");
+let companyStartDate = document.getElementById("start-date-p");
+let companyEndDate = document.getElementById("end-date-p");
+let companySummary = document.getElementById("summary-p");
 
 window.onload = () => {
   currentResume = data.resume[0];
@@ -10,7 +19,16 @@ window.onload = () => {
 };
 
 function updateResumeHtml(currentResume) {
-  appliedFor.innerHTML = currentResume.basics.name;
+  appliedFor.innerHTML = currentResume.basics.AppliedFor;
+  resumeName.innerHTML = currentResume.basics.name;
+  phoneNo.innerHTML = currentResume.basics.phone;
+  email.innerHTML = currentResume.basics.email;
+  linkedin.innerHTML = currentResume.basics.profiles.url;
+  companyName.innerHTML += " " + currentResume.work["Company Name"];
+  companyPosition.innerHTML += " " + currentResume.work.Position;
+  companyStartDate.innerHTML += " " + currentResume.work["Start Date"];
+  companyEndDate.innerHTML += " " + currentResume.work["End Date"];
+  companySummary.innerHTML += " " + currentResume.work.Summary;
   updateTechnicalSkills(currentResume.skills.keywords);
   updateHobbies(currentResume.interests.hobbies);
 }
